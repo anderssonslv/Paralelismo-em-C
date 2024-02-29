dd5350d5611b0adf994850f4e47bc39898dd4325
Andersson <89083903+anderssonslv@users.noreply.github.com>
2024-02-28 21:48:17 -0300

    Update 2
    Problema dos resultados determinísticos resolvidos!

--- pthreads_barbearia/src/main.c
+++ pthreads_barbearia/src/main.c
@@ -6,10 +6,15 @@
 #include <time.h>
 #include <unistd.h>
 
+// Problema dos resultados deterministicos resolvidos
+
 // OBS: Este código tem compatibilidade apenas com Linux no momento
 
-// Atendimentos sempre igual a(qtd de barbeiros + 1/2 qrd barbeiros)
-#define BARBERS_NUM 4 //default -> 4
+// 2 barbeiros -> Instavel, mesmo com poucos clientes ex 12
+// 4 barbeiros -> Estavel, com poucas desistências mesmo com 50 clientes
+// Se clientes menor ou igual barbeiros então 100% de atendimento como esperado
+
+#define BARBERS_NUM 2 //default -> 4
 void *barber_func(void *arg);
 void *client_func(void *arg);
 void haircut_duration();
@@ -27,15 +32,17 @@ sem_t client_sem;
 sem_t comb_sem;
 sem_t scissor_sem;
 sem_t waitchair_sem;
-pthread_mutex_t mutex;
-
+pthread_mutex_t clients_num_mut;
+pthread_mutex_t waitchair_num_mut;
+pthread_mutex_t haircuts_num_mut;
+pthread_mutex_t drops_num_mut;
 
 int main(int argc, char const *argv[]){
     system("clear");
     printf("\nBem vindo a Barbearia pthreads\n");
     srand(time(NULL));
-    clients_num = (rand() % 31) +1; // QTD clientes indefinida
-    //clients_num = 8;
+    //clients_num = (rand() % 31) +1; // QTD clientes indefinida
+    clients_num = 12;
     int total = clients_num;
     printf("QTD CLIENTES: %d\n",clients_num);
 
@@ -63,10 +70,11 @@ int main(int argc, char const *argv[]){
     printf("Clientes começam a chegar...\n"); 
     for (int i = 0; i < clients_num; i++){
         clients_id[i] = i;
+        client_entry();
         pthread_create(&clients_t[i],NULL,client_func,&clients_id[i]);
     }
     
-    /// Aguarda as últimas threads para sincronização
+    // Aguarda as últimas threads para sincronização
     for (int i = 0; i < BARBERS_NUM; i++)
         pthread_join(barbers_t[i],NULL);
 
@@ -79,85 +87,80 @@ int main(int argc, char const *argv[]){
     printf("Concluidos -> %d\n",haircuts);
     printf("Desistências -> %d\n",drops);
     printf("Obrigado e volte sempre!\n");
+    return 0;
 }
 
 void *barber_func(void *arg){
     int id = *((int *)arg);
 
-    while (clients_num > 0) {
-        printf("Barbeiro [%d] dorme...\n",id);
+    
+    while (haircuts + drops < clients_num) {
+        
+        
+        printf("Barbeiro [%d] esta dormindo...\n",id);
         sem_wait(&barber_sem);
 
-        // caso as últimos threads trancadas tenham sido liberadas pela qtd menor de clientes
-        if (clients_num < 1)
+        sem_wait(&comb_sem); // Se houver pente continua
+        sem_wait(&scissor_sem); // Se houver tesoura continua
+
+        if (haircuts + drops == clients_num)
             pthread_exit(NULL);
-        
-        //O barbeiro irá atender, cliente liberou este recurso
-        //Caso haja tesouras ele atende, se não houver ele espera
-        sem_wait(&comb_sem);
-        sem_wait(&scissor_sem);
 
-        printf("Barbeiro [%d] atende um cliente...\n",id);
-        //Cliente sai da cadeira de espera, e é atendido
-        sem_post(&waitchair_sem); //Libera a cadeira de espera
-
-        pthread_mutex_lock(&mutex);
+        printf("Barbeiro [%d] está fazendo o corte...\n",id);
+        pthread_mutex_lock(&waitchair_num_mut);        
         waitchair_num++;
+        pthread_mutex_unlock(&waitchair_num_mut); 
+        sem_post(&waitchair_sem); // Libera uma cadeira de espera para atender o cliente
+
+        haircut_duration(); // Faz o Corte
+        sem_post(&client_sem); // Libera o Cliente
+
+        pthread_mutex_lock(&haircuts_num_mut);        
         haircuts++;
-        clients_num--;
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&haircuts_num_mut);
 
-        printf("QTD CADEIRAS DE ESPERA: %d\n",waitchair_num);
-        
-        haircut_duration();
+        sem_post(&comb_sem); // Devolve os pentes
+        sem_post(&scissor_sem); // Devolve as tesouras
+        printf("Barbeiro [%d] devolve os recursos e finaliza!\n",id);
 
-        sem_post(&client_sem); // libera recurso para o cliente ir embora
-        
-        //após cortar o cabelo o barbeiro devolve os recursos de pente e tesoura
-        sem_post(&comb_sem); //Libera um pente
-        sem_post(&scissor_sem); //Libera uma tesoura
-
-        printf("QTD CLIENTES: %d\n",clients_num);
-        printf("Barbeiro [%d] devolve os materiais\n",id);
-        
-        // caso a qtd de clientes seja menor que os barbeiros, os ultimos barbeiros liberam os primeiros que estava dormindo
-        if (clients_num == 0) {
+        if (haircuts+drops == clients_num) // Caso tenha mais barbeiros
             for (int i = 0; i < BARBERS_NUM; i++)
-                sem_post(&barber_sem);
-        }
-    }   
+                sem_post(&barber_sem); // Este último barbeiro libera os que estão dormindo
+    }
 }
+
 
 void *client_func(void *arg){
     int id = *((int *)arg);
-    client_entry();
-    if (waitchair_num != 0){
-        pthread_mutex_lock(&mutex);
-        waitchair_num--;
-        pthread_mutex_unlock(&mutex);
-        printf("Cliente [%d] está na cadeira de espera...\n",id);
-        printf("QTD CADEIRAS DE ESPERA: %d\n",waitchair_num);
-        // Cliente libera o recurso para o barbeiro e vai para cadeira de espera
-        sem_post(&barber_sem); // Libera um recurso para o barbeiro
-        sem_wait(&waitchair_sem); // decrementa o recurso da cadeira do semáforo
-        sem_wait(&client_sem); // Cliente está cortando o cabelo, barbeiro deve liberar recurso
-        printf("O cliente vai embora feliz!\n");
-    } else {
-        printf("Cliente [%d] foi embora triste...\n",id);
-        pthread_mutex_lock(&mutex);
-        drops++;
-        clients_num--;
-        pthread_mutex_unlock(&mutex);
-        printf("QTD CLIENTES: %d\n",clients_num);
-        //sem_post(&barber_sem); // libera o recurso para um barbeiro que verifica se há mais clientes
-    }
-    pthread_exit(NULL);
-}
 
-void haircut_duration(){
+    if (waitchair_num > 0){
+        pthread_mutex_lock(&waitchair_num_mut);        
+        waitchair_num--;
+        pthread_mutex_unlock(&waitchair_num_mut);  
+        sem_post(&barber_sem); // Libera um barbeiro para atende-lo
+              
+        printf("Cliente [%d] aguarda sentado...\n",id);
+        sem_wait(&waitchair_sem); // Aguarda na cadeira até o barbeiro atender
+
+        printf("Cliente [%d] esta cortando o cabelo...\n",id);
+        sem_wait(&client_sem); // Está cortando o cabelo e o barbeiro irá libera-lo
+        
+        printf("O cliente [%d] foi embora feliz!\n",id);
+        printf("CORTES: %d\n",haircuts);
+    } else {
+        sem_post(&barber_sem);
+        printf("Cliente [%d] foi embora triste...\n",id);
+        pthread_mutex_lock(&drops_num_mut);
+        drops++;
+        pthread_mutex_unlock(&drops_num_mut);
+        printf("DESISTENCIAS: %d\n",drops);
+    }
+} 
+
+void haircut_duration(){ // tempo variado para cada corte
     sleep((rand() % 4)  + 3);
 }
 
 void client_entry() { // tempo variado para entrada dos clientes
-    sleep((rand() % 3)+3);
+    sleep((rand() % 3));
 }
